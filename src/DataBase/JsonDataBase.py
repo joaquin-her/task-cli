a16ea7cd810de8468071931ca33530b9f79cc8f6
Joaquin H <hernandez.joaquin346@gmail.com>
2025-03-26 10:08:03 -0300

    Docstrings aniadidos

--- src/DataBase/JsonDataBase.py
+++ src/DataBase/JsonDataBase.py
@@ -12,6 +12,8 @@ class JsonDataBase(IDataBase):
             self.data = self.createData(URI)
              
     def add(self, task_description:str):
+        """Aniade una tarea a la base de datos"""
+
         task = Task(self.getLastId(), task_description)
         self.data["tasks"].append(task.to_dict())
         self.data["items"] += 1
@@ -19,26 +21,36 @@ class JsonDataBase(IDataBase):
         pass
 
     def loadData(self, URI):
-            with open(URI, "r") as file:
-                data = file.read()
-                return json.loads(data)
-               
+        """Lee el archivo y carga sus datos"""
+        with open(URI, "r") as file:
+            data = file.read()
+            return json.loads(data)
+            
 
     def createData(self, URI):
-            with open(URI, "w") as file:
-                 data = {"tasks": [], "items":0}
-                 json.dump(data, file )
-            return data
+        """Crea el archivo vacio para guardar tareas si en la ruta no hay ningun elemento"""
+
+        with open(URI, "w") as file:
+            data = {"tasks": [], "items":0}
+            json.dump(data, file )
+        return data
 
     def saveData(self):
+        """Guarda su contenido de .data en la ruta de self.URI"""
         with open(self.URI, "w") as file:
-             json.dump(self.data, file, indent= 4)
+            json.dump(self.data, file, indent= 4)
 
 
     def getLastId(self):
+        """Devuelve el valor del ultimo Id de sus tareas"""
         return self.data["items"]
     
+    def getItems(self):
+        """Devuelve el listado de objetos almacenados"""
+        return self.data["tasks"]
+
     def getLast(self, limit):
+        """Devuelve los ultimos 'limit' elementos de la base de datos"""
         tasks = self.data["tasks"]
         if len(tasks) >= limit:
             return tasks[-limit:]  
