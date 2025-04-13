e7c34b0a4b2529b3c8837c2b93e1e8696e8595af
Joaquin H <hernandez.joaquin346@gmail.com>
2025-04-13 12:19:23 -0300

    added functions

--- task_cli.py
+++ task_cli.py
@@ -1,12 +1,25 @@
 import sys 
+from Task import Task
 
-from src.CLI import CLI
-from src.DataBase.JsonDataBase import JsonDataBase
+first_argument = sys.argv[1]
 
-saveFilePath = 'tasks.json'
-database = JsonDataBase(saveFilePath)
+saveFilePath = './data/tasks.json'
+dataBase = JsonDataBase(saveFilePath)
 
-cli = CLI(database)
+def addTask(data_path:str, task_description:str): 
+	newTask = Task(1, task_description)
+	dataBase.add(newTask)
 
-cli.handle(sys.argv[1:])
+
+
+def handle_list():
+	return 'La lista esta vacia'
+
+
+match first_argument:
+	case "add":
+		print( addTask(saveFilePath, sys.argv[2]))
+	case 'list':
+		print(handle_list())
+
 
