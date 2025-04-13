fed41532fd97613e37a05e513faa7369c471cccc
Joaquin H <hernandez.joaquin346@gmail.com>
2025-04-13 16:42:49 -0300

    updateData and __str__ implemented

--- src/Task/Task.py
+++ src/Task/Task.py
@@ -19,22 +19,58 @@ class Task(object):
 		}
 		return dict
 	
-	def setStatus(self, status:str):
+	def setStatus(self, status: str):
+		if not isinstance(status, str):
+			raise TypeError("El argumento 'status' debe ser de tipo str")
 		if status in STATUS_VALUES:
 			self.status = status
-			self.updated = datetime.now().isoformat()
+			self.updateDatetime()
 			return
 		raise UnknownStatusException(status)
 
+	def setDescription(self, description: str):
+		if not isinstance(description, str):
+			raise TypeError("El argumento 'description' debe ser de tipo str")
+		self.description = description
+		self.updateDatetime()
+		return
+
 	@classmethod
-	def fromDict(cls, dict: dict) :
+	def fromDict(cls, dict: dict)-> dict :
 		task = Task(dict["description"])
 		for key, value in dict.items():
 			if hasattr(task, key):
 				setattr(task, key, value)
 		return task
 	
+	def update(self, field:str, value:str):
+		if (field == "description"):
+			self.setDescription(value)
+		elif (field == "status"):
+			self.setStatus(value)
+		else:
+			raise UnknownFieldException(field)
+
+	def updateDatetime(self):
+		""" Actualiza el campo 'updated' a este instante """
+		self.updated = datetime.now().isoformat()
+
+	def __str__(self):
+		""" Devuelve formateado a string el objeto Task """
+		return (
+			f"Task:\n"
+			f"  Description: {self.description}\n"
+			f"  Status: {self.status}\n"
+			f"  Created: {self.created}\n"
+			f"  Updated: {self.updated}"
+		)
+	
 class UnknownStatusException(Exception):
+	"""Excepcion que se lanza cuando se ingresa un Status desconocido"""
+	def __init__(self, status):
+		super().__init__(f"El status '{status}' no es valido o es deconocido ")
+		
+class UnknownFieldException(Exception):
 	"""Excepcion que se lanza cuando se ingresa un Status desconocido"""
 	def __init__(self, status):
 		super().__init__(f"El status '{status}' no es valido o es deconocido ")
