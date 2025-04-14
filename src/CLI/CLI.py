bc1298e249d1a15668e4daf48f1041d4ca82475a
Joaquin H <hernandez.joaquin346@gmail.com>
2025-04-14 17:22:23 -0300

    cli commands improvement

--- src/CLI/CLI.py
+++ src/CLI/CLI.py
@@ -1,13 +1,25 @@
+""" CLI.py
+    This module contains the CLI class, which is responsible for handling the command line interface of the application.
+    It takes a list of arguments and executes the corresponding command. The commands are as follows:
+    - list: Lists all tasks or filters them based on the provided argument.
+    - add: Adds a new task to the database.
+    - mark-in-progress: Marks a task as in-progress.
+    - mark-done: Marks a task as done.
+    - update: Updates a task's field with the provided value.
+    - delete: Deletes a task from the database.
+"""
+
+
 from typing import List
 from ..DataBase import IDataBase
 from ..Task import Task
 class CLI(object):
-    """ Creates a CLI Ibject with a data source to save and  """
     def __init__(self, database: IDataBase):
+        """ Creates a CLI Ibject with a data source to save and  """
         self.database = database
     
-    """ handles the console args. Args must be a list of Strings """
     def handle(self, args: List[str]):
+        """ handles the console args. Args must be a list of Strings """
         argument_amount = len(args)
         if argument_amount > 0 :
             command = args[0]
@@ -23,11 +35,11 @@ class CLI(object):
             case 4:
                 self.executeCommand(command, args[1], args[2], args[3])
 
-    """ determines and executes a command"""
     def executeCommand(self, command: str, first_arg:str='10', second_arg:str='', third_arg:str=''):
+        """ Determines and executes a command"""
         match command:
             case 'list':
-                self.listTasks(first_arg)
+                self.handleListCommand(first_arg)
             case 'add':
                 self.addTask(first_arg)
             case 'mark-in-progress':
@@ -36,18 +48,57 @@ class CLI(object):
                 self.updateTask(first_arg, 'status', 'done')
             case 'update':
                 self.updateTask(first_arg, second_arg, third_arg)
+            case 'delete':
+                self.deleteTask(first_arg)
+            case default:
+                raise ValueError(f"Comando desconocido: {command}")
+
 
     def updateTask(self, id:str, field:str, value:str):
+        """ Updates a task in the database """
         self.database.update(id, field, value)
 
-    """ appends a task to the database"""
     def addTask(self, task_description:str): 
+        """ appends a task to the database"""
         self.database.add(task_description)
         print(task_description + ": aniadida correctamente")
         
-    """ lists the last 10 tasks in the database"""
-    def listTasks(self, amount):
-        items = self.database.getLast(int(amount))
+    def handleListCommand(self, first_arg:str):
+        """ Handles the list command """
+        if not first_arg.isnumeric():
+            self.filterTasks(first_arg)
+        elif first_arg == '':
+            self.listAllTasks()
+        elif first_arg > '0':
+            self.listLastTasks(first_arg)
+        else:
+            raise ValueError("El argumento no es valido")
+
+    def listLastTasks(self, limit:str):
+        """ Lists the last tasks in the database """
+        items = self.database.getLast(int(limit))
+        self.printTasks(items)
+
+    def listAllTasks(self):
+        """ Lists all the tasks in the database """
+        items = self.database.getItems()
+        self.printTasks(items)
+
+    def filterTasks(self, filter:str):
+        """ Filters the tasks in the database """
+        items = self.database.getItems(filter)
+        if len(items) == 0:
+            print("No se han encontrado tareas")
+            return
+        self.printTasks(items)
+
+    def printTasks(self, items:List[dict]):
+        """ Prints the tasks in the database """
         for item in items:
             print(f"\n{item["id"]}:\t")
             print(Task.fromDict(item))
+        
+    def deleteTask(self, id:str):
+        self.database.removeItem(int(id))
+        print(f"Tarea {id} eliminada correctamente")
+
